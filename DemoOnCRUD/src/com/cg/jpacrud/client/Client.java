ff07c19d4a1e1f7d64b45a4f526dbca6e3ca72b6
Toshiba <Toshiba@Toshiba-PC>
2022-08-26 12:18:04 +0530

    hibernate

--- DemoOnCRUD/src/com/cg/jpacrud/client/Client.java
+++ DemoOnCRUD/src/com/cg/jpacrud/client/Client.java
@@ -1,5 +1,52 @@
 package com.cg.jpacrud.client;
 
-public class Client {
+import com.cg.jpacrud.entities.Student;
+import com.cg.jpacrud.service.StudentService;
+import com.cg.jpacrud.service.StudentServiceImpl;
+
+
+public class Client 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		// CRUD operation calling activity
+		Student student = new Student();
+		
+		StudentServiceImpl service = new StudentServiceImpl();
+		
+		// Create Operation
+		
+		//student.setStudentid(1);
+	//student.setName("Avantika ");
+	//service.addStudent(student);
+		
+		
+	// Retrieve Operation
+	//	service.findStudentById(104);
+		//System.out.println("ID is: "+student.getStudentid());
+		//System.out.println("Name is: "+student.getName());
+		
+		
+		
+		//Update Operation
+		student  =service.findStudentById(1);
+		student.setName("Avantika ");
+		service.updateStudent(student);
+		
+		
+	/*	service.findStudentById(101);
+		System.out.println("ID is: "+student.getStudentid());
+		System.out.println("Name is: "+student.getName());
+		
+		
+		// Delete Operation
+		student=service.findStudentById(104);
+		service.removeStudent(student);
+		System.out.println("Row removed");*/
+		
+		
+	}
 
 }
--- DemoOnCRUD/src/com/cg/jpacrud/dao/JPAUtil.java
+++
@@ -1,5 +0,0 @@
-package com.cg.jpacrud.dao;
-
-public class JPAUtil {
-
-}
